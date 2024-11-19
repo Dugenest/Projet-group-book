5d3276b364540a5a51ae78f469572e38bc1c94bb
Dugenest <sebastien.dugenest@gmail.com>
2024-11-19 09:58:13 +0100

    first commit initial

---
+++ src/test/java/com/afci/projet_spring_book/ProjetSpringBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afci.projet_spring_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
