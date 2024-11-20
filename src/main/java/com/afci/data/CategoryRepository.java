293ee7f3371d56f4daf53a3a1dba210618cdc13c
Dugenest <sebastien.dugenest@gmail.com>
2024-11-20 14:37:08 +0100

    update code category

--- src/main/java/com/afci/data/CategoryRepository.java
+++ src/main/java/com/afci/data/CategoryRepository.java
@@ -1,6 +1,9 @@
 package com.afci.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface CategoryRepository extends CrudRepository<Category, Long>{
 
